51a115c0eb46ab6093412776517e9cc3b691d93c
Marcelo Avanzi <marcelodaniel@hotmail.com>
2023-05-31 20:30:24 -0300

    Consuming external API

--- Backend/Routes/meal.routes.js
+++ Backend/Routes/meal.routes.js
@@ -1,8 +1,10 @@
 module.exports = (app) => {
     const mealController = require("../Controllers/meal.controller");
+    const suggesticController = require("../Controllers/suggesticAPI.controller");
 
     app.get('/api/getAllMeals', mealController.getAllMeals);
     app.get('/api/getByName/:name', mealController.getByName);
     app.post('/api/createMeal', mealController.createMeal);
+    app.post('/api/suggesticMeals', suggesticController.getMealPlans);
 
 };
