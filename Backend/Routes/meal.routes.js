ff171c05117374106c204f61c4006e6e1bf92261
Marcelo Avanzi <marcelodaniel@hotmail.com>
2023-06-01 20:14:42 -0300

    excluding external api due to security reasons

--- Backend/Routes/meal.routes.js
+++ Backend/Routes/meal.routes.js
@@ -1,10 +1,8 @@
 module.exports = (app) => {
     const mealController = require("../Controllers/meal.controller");
-    const suggesticController = require("../Controllers/suggesticAPI.controller");
 
     app.get('/api/getAllMeals', mealController.getAllMeals);
     app.get('/api/getByName/:name', mealController.getByName);
     app.post('/api/createMeal', mealController.createMeal);
-    app.post('/api/suggesticMeals', suggesticController.getMealPlans);
 
 };
