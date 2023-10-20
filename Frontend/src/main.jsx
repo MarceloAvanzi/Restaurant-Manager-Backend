031bf191a6153ac886e306377cda9d59d96be51d
Marcelo Avanzi <marcelodaniel@hotmail.com>
2023-10-19 21:38:25 -0300

    Restructuring pages and components

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/MainPage/index.jsx'
+import App from './App.jsx'
 import './static/styles.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -11,7 +11,7 @@ export default {
     },
     extend: {
       backgroundImage: {
-        'background-meal': "url('./src/public/assets/main-background.jpg')",
+        'background-meal': "url('./assets/main-background.jpg')",
       }
     },
   },
